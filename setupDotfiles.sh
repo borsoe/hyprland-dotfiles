cccf42fbefca7a305c26a8f3510d4137d30cd6be
borsoe <borsoe.reza@gmail.com>
2023-05-20 21:15:21 +0330

    initial commit: setup bare dotfiles and added installer using stow

---
+++ setupDotfiles.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "checking to see if stow is installed"
+
+CONFIG_LOCATION=$HOME/.config/
+
+if [[ -x "$(command -v stow)" ]]; then
+  echo "found stow, proceding to link dotfiles "
+  else
+    command
+    echo "couldn't find stow, exiting the linker"
+    exit
+fi
+
+for PACK in fish hypr kitty omf pulse
+do
+  echo "stowing $PACK ..."
+  stow --adopt --target=$CONFIG_LOCATION/$PACK --dir=.config/ $PACK 
+done
+
