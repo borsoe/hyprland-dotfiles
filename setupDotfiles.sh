c976c91f11b6ff0e89c9b52ae3bbf85be468bf1a
borsoe <borsoe.reza@gmail.com>
2023-05-22 17:01:24 +0330

    tweak: made PACKS into an array of strings

--- setupDotfiles.sh
+++ setupDotfiles.sh
@@ -4,6 +4,8 @@ echo "checking to see if stow is installed"
 
 CONFIG_LOCATION=$HOME/.config/
 
+PACKS=("fish" "hypr" "kitty" "omf" "pulse" "neofetch" "wofi" "mako" "swaylock" "wlogout")
+
 if [[ -x "$(command -v stow)" ]]; then
   echo "found stow, proceding to link dotfiles "
   else
@@ -13,11 +15,11 @@ if [[ -x "$(command -v stow)" ]]; then
 fi
 
 echo "checking if the required directories exist"
-for PACK in fish hypr kitty omf pulse neofetch wofi mako swaylock wlogout
+for PACK in ${PACKS[@]}
 do
   ([ -d "$CONFIG_LOCATION$PACK" ] && echo "Directory $CONFIG_LOCATION$PACK exists!") || (echo "Couldn't find directory $CONFIG_LOCATION/$PACK ! Attempting to create..." && mkdir $CONFIG_LOCATION/$PACK)
 done
-for PACK in fish hypr kitty omf pulse neofetch wofi mako swaylock wlogout
+for PACK in ${PACKS[@]}
 do
   echo "stowing $PACK ..."
   stow --adopt --target=$CONFIG_LOCATION$PACK --dir=.config/ $PACK 
